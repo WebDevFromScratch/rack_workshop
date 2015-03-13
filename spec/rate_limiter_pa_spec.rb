44f94866c5dc221c9e1649df9e02f599ca0e6236
PIOTR KLOSINSKI <mybettingstuff@gmail.com>
2015-03-13 17:04:54 +0100

    re-factored tests written so far

--- spec/rate_limiter_pa_spec.rb
+++ spec/rate_limiter_pa_spec.rb
@@ -5,19 +5,16 @@ describe Rack::RateLimiterPa do
   let(:app) { lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']]} }
   let(:stack) { Rack::Lint.new(Rack::RateLimiterPa.new(app)) }
   let(:request) { Rack::MockRequest.new(stack) }
+  let(:response) { request.get('/') }
 
   context 'issue a request' do
-    let(:response) { request.get('/') }
-
     it 'response should be okay' do
       expect(response).to be_ok
     end
   end
 
   context 'X-RateLimit-Limit header' do
-    it 'is a nil if not set' do
-      response = request.get('/')
-
+    it 'is a string "0" if not set' do
       expect(response.headers['X-RateLimit-Limit']).to eq('0')
     end
 
@@ -31,21 +28,18 @@ describe Rack::RateLimiterPa do
   end
 
   context 'X-RateLimit-Remaining header' do
-    it 'is present' do
-      stack = Rack::Lint.new(Rack::RateLimiterPa.new(app, { limit: '60' }))
-      request = Rack::MockRequest.new(stack)
-      response = request.get('/')
+    let(:stack) { Rack::Lint.new(Rack::RateLimiterPa.new(app, { limit: '60' })) }
+    let(:request) { Rack::MockRequest.new(stack) }
+    let(:response) { request.get('/') }
 
+    it 'is present' do
       expect(response.headers['X-RateLimit-Remaining']).to be_truthy
     end
 
     it 'decreases by one with each request' do
-      stack = Rack::Lint.new(Rack::RateLimiterPa.new(app, { limit: '60' }))
-      request = Rack::MockRequest.new(stack)
-      request.get('/')
-      response = request.get('/')
+      2.times { request.get('/') }
 
-      expect(response.headers['X-RateLimit-Remaining'].to_i).to eq(58)
+      expect(response.headers['X-RateLimit-Remaining'].to_i).to eq(57)
     end
   end
 end
