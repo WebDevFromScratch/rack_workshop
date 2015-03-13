2d8e3bade3320476d7ef99b1f7191a80cf666622
PIOTR KLOSINSKI <mybettingstuff@gmail.com>
2015-03-13 16:09:57 +0100

    [#2] added dynamic header setting with options

--- lib/rate_limiter_pa.rb
+++ lib/rate_limiter_pa.rb
@@ -1,13 +1,14 @@
 require "rate_limiter_pa/version"
 
 class RateLimiterPa
-  def initialize(app)
+  def initialize(app, options={})
     @app = app
+    @options = options
   end
 
   def call(env)
     status, headers, response = @app.call(env)
-    headers.merge! 'X-RateLimit-Limit' => 60
+    headers.merge! 'X-RateLimit-Limit' => @options[:limit]
     [status, headers, response]
   end
 end
--- spec/rate_limiter_pa_spec.rb
+++ spec/rate_limiter_pa_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe RateLimiterPa do
   let(:app) { lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']]} }
-  let(:stack) { RateLimiterPa.new(app) }
+  let(:stack) { RateLimiterPa.new(app, {}) }
   let(:request) { Rack::MockRequest.new(stack) }
 
   context 'issue a request' do
@@ -12,9 +12,20 @@ describe RateLimiterPa do
     it 'response should be okay' do
       expect(response).to be_ok
     end
+  end
 
-    it 'has a header present' do
-      puts response.headers
+  context 'X-RateLimit-Limit header' do
+    it 'is a nil if not set' do
+      response = request.get('/')
+
+      expect(response.headers['X-RateLimit-Limit']).to be(nil)
+    end
+
+    it 'is equal to a passed value if set' do
+      stack = RateLimiterPa.new(app, { limit: 60 })
+      request = Rack::MockRequest.new(stack)
+      response = request.get('/')
+
       expect(response.headers['X-RateLimit-Limit']).to eq(60)
     end
   end
